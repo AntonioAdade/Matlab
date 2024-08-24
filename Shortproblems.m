7add57b9da19f799e8911907719b49a0eb08e7a3
AntonioAdade <119971014+AntonioAdade@users.noreply.github.com>
2024-08-23 23:54:01 -0300

    First exercises list
    list of basics commands to initial level students

---
+++ Shortproblems.m
@@ -0,0 +1,38 @@
+% mensagem de olá mundo
+display('Hello World! I am going to learn Matlab')
+
+% 1 - Scalar variables
+a = 10;
+b = 2.5*10^23;
+c = 3+3i;
+d = exp((1i*2*pi)/3);
+
+% 2 - Vector variables
+avec = [pi 15 9 26];
+bvec = [2.71;8;28;182];
+cvec = (5:-0.2:-5);
+dvec = logspace(0,1,100);
+evec = 'Hello World';
+
+% 3 - Matrix variables
+amat = ones(9)*2;
+bl = [1 2 3 4 5 4 3 2 1];
+bmat = diag(bl);
+cl = 1:100;
+cmat = reshape(cl,[10,10]);
+dmat = NaN([3,4]);
+emat = [13 -1 5; -22 10 -87];
+fmat = randi([-3,3],[5,3]);
+
+% 4 - Scalar equations
+
+
+
+
+
+
+
+
+
+
+ 
